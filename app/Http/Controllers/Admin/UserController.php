9df573d0203ede2e41d74fc2c82c0852b3e54250
deJames-13 <de.james013@gmail.com>
2025-07-02 12:15:30 +0800

    full name remove

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -653,7 +653,7 @@ class UserController extends Controller
             $query->where(function($q) use ($search) {
                 $q->where('first_name', 'like', '%' . $search . '%')
                   ->orWhere('last_name', 'like', '%' . $search . '%')
-                  ->orWhere('full_name', 'like', '%' . $search . '%')
+                //   ->orWhere('full_name', 'like', '%' . $search . '%')
                   ->orWhere('email', 'like', '%' . $search . '%')
                   ->orWhere('employee_id', 'like', '%' . $search . '%');
             });
